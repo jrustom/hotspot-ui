a6a17d6ee4beacce0a73af341fefff0066e48092
Jason Rustom <jasonrustom04@gmail.com>
2025-07-07 23:24:43 -0400

    added some signup logic

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -6,7 +6,28 @@ type UserResponseDto = {
 
 
 export async function handleSignUp(name: string, email: string, password: string) {
+  try {
+    const response = await fetch("http://localhost:8080/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
 
+    if (!response.ok) {
+      const error = await response.json();
+      console.log(error);
+    }
+
+    const userResponse: UserResponseDto = await response.json();
+
+    console.log(userResponse.id);
+    console.log(userResponse.name);
+    console.log(userResponse.email);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function handleLogin(email: string, password: string) {
