54fa06510f9f3f4e935edc96f0c099cd951fb10b
Jason Rustom <jasonrustom04@gmail.com>
2025-07-10 22:30:17 -0400

    added login to main component with blur and it disappears when you're logged in

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import AccountRegistration from "./components/accountRegistration";
+import MainComponent from "./components/mainComponent";
+import { useAuth } from "./contexts/AuthContext";
 
 function App() {
+  const { userData } = useAuth();
+  const [showRegistration, setShowRegistration] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (userData != null) setShowRegistration(false);
+  }, [userData]);
+
   return (
     <>
-      {/* <Test />  */}
-      <AccountRegistration />
+      <div
+        className={`fixed inset-0 flex items-center justify-center bg-opacity-50 backdrop-blur-sm z-1 ${
+          showRegistration ? "" : "hidden"
+        }`}
+      >
+        <AccountRegistration />
+      </div>
+      <div className="flex-1">
+        <MainComponent />
+      </div>
     </>
   );
 }
---
+++ src/components/mainComponent.tsx
@@ -0,0 +1,10 @@
+function MainComponent() {
+  return (
+    <div className="bg-blue-500 min-h-screen w-full">
+      {/* Your content goes here */}
+      <h1 className="text-white p-8">Main Component</h1>
+    </div>
+  );
+}
+
+export default MainComponent;
